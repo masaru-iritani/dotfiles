c226ac56cf1bcdc1adccc1ce050e056861c0c8a5
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2024-10-28 00:53:51 +0900

    Add more Git aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,4 @@
-# See also https://fishshell.com/docs/current/interactive.html#command-line-editor
+# https://fishshell.com/docs/current/interactive.html#command-line-editor
 function fish_hybrid_key_bindings --description \
     "Vi-style bindings that inherit emacs-style bindings in all modes"
     for mode in default insert visual
@@ -9,11 +9,17 @@ end
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    abbr --add ghg git --no-pager log -n 1 --format=%H%n --grep
+    abbr --add glom git log origin/main..HEAD
+    abbr --add gnom git log --name-only origin/main..HEAD
+    abbr --add reload source ~/.config/fish/config.fish
+
     set -g fish_key_bindings fish_hybrid_key_bindings
     set fish_cursor_default block blink
     set fish_cursor_insert line blink
     set fish_cursor_replace underscore blink
     set fish_cursor_replace_one underscore blink
     set fish_tmux_autostart true
+
     starship init fish | source
 end
