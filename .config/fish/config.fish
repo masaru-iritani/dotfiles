26dab398b0f5db606f984be84233ce1785c0d1b3
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2024-10-28 00:42:31 +0900

    Set Fish hybrid key bindings

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,19 @@
+# See also https://fishshell.com/docs/current/interactive.html#command-line-editor
+function fish_hybrid_key_bindings --description \
+    "Vi-style bindings that inherit emacs-style bindings in all modes"
+    for mode in default insert visual
+        fish_default_key_bindings -M $mode
+    end
+    fish_vi_key_bindings --no-erase
+end
+
 if status is-interactive
     # Commands to run in interactive sessions can go here
+    set -g fish_key_bindings fish_hybrid_key_bindings
+    set fish_cursor_default block blink
+    set fish_cursor_insert line blink
+    set fish_cursor_replace underscore blink
+    set fish_cursor_replace_one underscore blink
     set fish_tmux_autostart true
     starship init fish | source
 end
