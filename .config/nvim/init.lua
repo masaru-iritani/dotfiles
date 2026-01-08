a8b66219dee88887b4f01e7a1ffadba3a29701cf
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2026-01-08 09:05:18 +0900

    Install the LazyVim Starter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,2 @@
-local vim_path = vim.fn.expand("~/.vim")
-
-vim.opt.runtimepath:prepend(vim_path)
-vim.opt.runtimepath:append(vim_path .. "/after")
-vim.opt.packpath = vim.opt.runtimepath:get()
-
-vim.cmd("source " .. vim.fn.expand("~/.vimrc"))
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
