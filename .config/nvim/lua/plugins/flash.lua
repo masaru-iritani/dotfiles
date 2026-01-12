39788f355099c1e7a885799d79ed386455cdacbd
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2026-01-12 10:00:04 +0900

    Swap colon and semicolon in LazyVim

--- .config/nvim/lua/plugins/flash.lua
+++ .config/nvim/lua/plugins/flash.lua
@@ -4,4 +4,11 @@ return {
     -- disable the default flash keymap
     { "s", mode = { "n", "x", "o" }, false },
   },
+  opts = {
+    modes = {
+      char = {
+        keys = { "f", "F", "t", "T", [";"] = ":", "," },
+      },
+    },
+  },
 }
