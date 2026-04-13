a7e90accd7ba9fbbb0ff8c005976284f56d3c285
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2026-04-14 08:24:26 +0900

    Force the LazyVim project root to be the repository

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,6 +1,11 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+-- Force the project root to be the repository root. This prevents LazyVim
+-- from diving into individual Go modules or workspaces within monorepo.
+vim.g.root_spec = { ".git" }
+
 vim.opt.guicursor = {
   "n-v-c-sm:blinkon500-blinkoff500",
   "i-ci-ve:ver50-blinkon500-blinkoff500",
