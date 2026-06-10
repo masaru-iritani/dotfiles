dbcb9ae8725108ebb25396e018e4e07b4a41929b
Masaru Iritani <25241373+masaru-iritani@users.noreply.github.com>
2026-06-10 15:22:27 +0900

    Close Wezterm on clean exit

--- .wezterm.lua
+++ .wezterm.lua
@@ -3,12 +3,13 @@ local config = wezterm.config_builder()
 
 config.cursor_blink_ease_in = "Constant"
 config.cursor_blink_ease_out = "Constant"
-config.initial_cols = 120
-config.initial_rows = 24
-config.font_size = 16
+config.exit_behavior = "CloseOnCleanExit"
 config.font = wezterm.font_with_fallback({
 	"UDEV Gothic 35NF",
 	"Symbols Nerd Font Mono",
 })
+config.font_size = 16
+config.initial_cols = 120
+config.initial_rows = 24
 config.notification_handling = "AlwaysShow"
 return config
